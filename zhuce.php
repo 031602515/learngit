fe573b2c6a7a9cf39a810688a04d861153695aaf
031602515 <hxy-08@qq.com>
2016-12-20 23:06:32 +0800

    Update zhuce.php

--- zhuce.php
+++ zhuce.php
@@ -10,61 +10,38 @@
 <center>
 <body style="background-color:RosyBrown;"> 
 <h1>来注册！</h1>
-<?php
-$nameErr = $password="";
-$name = $passwordErr= "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    if (empty($_POST["name"]))
-    {
-        $nameErr = "名字是必需的";
-    }
-    else
-    {
-        $name = test_input($_POST["name"]);
-       
-        if (!preg_match("/^[a-zA-Z ]*$/",$name))
-        {
-            $nameErr = "只允许字母和空格"; 
-        }
-    }
-    
-
-	if (empty($_POST["password"]))
-	{
-		$nameErr = "密码是必需的";
-	}
-	else
-	{
-		$password = test_input($_POST["password"]);
-	}
-}
-
-function test_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-?>
 
 
 <form method="post" action="in.php"> 
 
- <p>  名字: <input type="text" name="name" value="<?php echo $name;?>">
-   <span class="error">* <?php echo $nameErr;?></span>
+ <p>  名字: <input type="text" name="name" >
+   <span class="error">*</span>
  </p>
  
  <p> 
    密码：<input type="password" name="password">
-	<span class="error">* <?php echo $passwordErr;?></span>
+	<span class="error">* </span>
  </p>
 	
    <input type="submit" name="submit" value="提交"> 
 </form>
+ <?php
+if (isset($_REQUEST['submit']))
+{
+    if (empty($_POST["name"]))
+    {
+      echo" <script>alert('用户名不能为空!')</script>";
+    }
+ 
+    
 
+	if (empty($_POST["password"]))
+	{
+		
+      echo" <script>alert('密码不能为空!')</script>";
+	}
+}
+?>
 
 </body>
 </html>
