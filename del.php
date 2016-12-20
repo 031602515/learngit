8be8ef8b0faebb2e7feb6cbf7e86e63d0a6faaf2
031602515 <hxy-08@qq.com>
2016-12-20 22:56:15 +0800

    Update del.php

---
+++ del.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>删除留言</title>
+</head>
+<center>
+<h1>删掉留言</h1>
+
+
+<body style="background-color:pink;">
+<form action="delin.php"method="get">
+删除留言的id：
+<input type="text" name="id"/>
+<input type="submit" name="sub" value="删除" />
+</form>
+<p >提示：请正确选择留言的id，删除可能不能撤回:(</p>
+</br>
+<a href= seebook.php> 查看留言板</a>
+</body>
+
+
+</html>
